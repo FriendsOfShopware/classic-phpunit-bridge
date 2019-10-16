e32a2f2eb4fc8c01146a567d2260d57afab0a385
Soner Sayakci <s.sayakci@shopware.com>
2019-10-16 15:51:52 +0200

    Add composer support

---
+++ src/Bootstrap/Functional/NormalKernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Frosh\ClassicPhpunitBridge\Bootstrap\Functional;
+
+use Shopware\Models\Shop\Shop;
+
+class NormalKernel extends \Shopware\Kernel
+{
+    /**
+     * Static method to start boot kernel without leaving local scope in test helper
+     */
+    public static function start()
+    {
+        $kernel = new self('testing', true);
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        $container->get('plugins')->Core()->ErrorHandler()->registerErrorHandler(E_ALL | E_STRICT);
+
+        /** @var \Shopware\Models\Shop\Repository $repository */
+        $repository = $container->get('models')->getRepository(Shop::class);
+
+        $shop = $repository->getActiveDefault();
+        if (Shopware()->Container()->has('shopware.components.shop_registration_service')) {
+            Shopware()->Container()->get('shopware.components.shop_registration_service')->registerShop($shop);
+        } else {
+            $shop->registerResources();
+        }
+
+        $_SERVER['HTTP_HOST'] = $shop->getHost();
+    }
+
+    protected function getConfigPath()
+    {
+        return __DIR__ . '/config.php';
+    }
+}
--- src/Bootstrap/Functional/bootstrap.php
+++ src/Bootstrap/Functional/bootstrap.php
@@ -20,41 +20,18 @@ while (true) {
 }
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Shopware\Models\Shop\Shop;
+use Dotenv\Dotenv;
 
-class TestKernel extends \Shopware\Kernel
-{
-    /**
-     * Static method to start boot kernel without leaving local scope in test helper
-     */
-    public static function start()
-    {
-        $kernel = new self('testing', true);
-        $kernel->boot();
-
-        $container = $kernel->getContainer();
-        $container->get('plugins')->Core()->ErrorHandler()->registerErrorHandler(E_ALL | E_STRICT);
-
-        /** @var \Shopware\Models\Shop\Repository $repository */
-        $repository = $container->get('models')->getRepository(Shop::class);
-
-        $shop = $repository->getActiveDefault();
-        if (Shopware()->Container()->has('shopware.components.shop_registration_service')) {
-            Shopware()->Container()->get('shopware.components.shop_registration_service')->registerShop($shop);
-        } else {
-            $shop->registerResources();
-        }
-
-        $_SERVER['HTTP_HOST'] = $shop->getHost();
-    }
-
-    protected function getConfigPath()
-    {
-        return __DIR__ . '/config.php';
-    }
+$kernel = \Frosh\ClassicPhpunitBridge\Bootstrap\Functional\NormalKernel::class;
+if (file_exists($searchDirectory . '/.env')) {
+    $dotenv = Dotenv::create($searchDirectory);
+    $dotenv->load();
+    $kernel = \Frosh\ClassicPhpunitBridge\Bootstrap\Functional\ComposerKernel::class;
 }
+define('SEARCH_DIRECTORY', $searchDirectory);
+
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-TestKernel::start();
+$kernel::start();
 
