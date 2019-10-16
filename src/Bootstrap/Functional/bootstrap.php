f947af715edb70cee7f7af4f84e1ebd2f92e8036
Soner Sayakci <s.sayakci@shopware.com>
2019-10-16 15:28:03 +0200

    Fix Functional on non Git projects

--- src/Bootstrap/Functional/bootstrap.php
+++ src/Bootstrap/Functional/bootstrap.php
@@ -3,8 +3,9 @@
 define('TESTS_RUNNING', true);
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 $_SERVER['SERVER_REQUEST_METHOD'] = 'GET';
+$loader = null;
 
-$searchDirectory = dirname(dirname(__DIR__));
+$searchDirectory = dirname(__DIR__, 7);
 while (true) {
     $newSearchDirectory = realpath($searchDirectory . DIRECTORY_SEPARATOR . '..');
     if ($searchDirectory === false || strlen($searchDirectory) < 3 || $newSearchDirectory === $searchDirectory) {
@@ -12,8 +13,44 @@ while (true) {
     }
     $searchDirectory = $newSearchDirectory;
 
-    if (file_exists($autoloadFile = implode(DIRECTORY_SEPARATOR, [$searchDirectory, 'tests', 'Functional', 'bootstrap.php']))) {
-        require $autoloadFile;
+    if (file_exists($autoloadFile = implode(DIRECTORY_SEPARATOR, [$searchDirectory, 'vendor', 'autoload.php']))) {
+        $loader = require $autoloadFile;
         break;
     }
 }
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Shopware\Models\Shop\Shop;
+
+class TestKernel extends \Shopware\Kernel
+{
+    /**
+     * Static method to start boot kernel without leaving local scope in test helper
+     */
+    public static function start()
+    {
+        $kernel = new self('testing', true);
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        $container->get('plugins')->Core()->ErrorHandler()->registerErrorHandler(E_ALL | E_STRICT);
+
+        /** @var \Shopware\Models\Shop\Repository $repository */
+        $repository = $container->get('models')->getRepository(Shop::class);
+
+        $shop = $repository->getActiveDefault();
+        Shopware()->Container()->get('shopware.components.shop_registration_service')->registerShop($shop);
+
+        $_SERVER['HTTP_HOST'] = $shop->getHost();
+    }
+
+    protected function getConfigPath()
+    {
+        return __DIR__ . '/config.php';
+    }
+}
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+TestKernel::start();
+
